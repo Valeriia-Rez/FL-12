d11526f450689b7b320f47637a8ff23986c14bdf
Valeriia-Rez <Valeria09122016@gmail.com>
2019-12-24 16:00:08 +0200

    implement homework N6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,37 +1,39 @@
 let discriminant, x, x1, x2, a, b, c;
 const sides = 4;
 const quadrat = 2;
+const promptMessage = "Enter number for";
+const errorMessage = "Invalid input data";
 
-if (confirm('Solve Quadratic equation. Example: ax2 + bx + c = 0')) {
-    a = parseFloat(prompt('Enter number for a ='));
+if (confirm("Solve Quadratic equation. Example: ax2 + bx + c = 0")) {
+    a = parseFloat(prompt(`${promptMessage} a =`));
     if (a === 0 || isNaN(a)) {
-        alert('Invalid input data');
-        console.log('Invalid input data');
+        alert(errorMessage);
+        console.log(errorMessage);
     } else {
-        b = parseFloat(prompt('Enter number for b ='));
+        b = parseFloat(prompt(`${promptMessage} b =`));
         if (isNaN(b)) {
-            alert('Invalid input data');
-            console.log('Invalid input data');
+            alert(errorMessage);
+            console.log(errorMessage);
         } else {
-            c = parseFloat(prompt('Enter number for c ='));
+            c = parseFloat(prompt(`${promptMessage} c =`));
             if (isNaN(c)) {
-                alert('Invalid input data');
-                console.log('Invalid input data');
+                alert(errorMessage);
+                console.log(errorMessage);
             } else {
-                alert(a + 'x2' + '+' + b + 'x' + '+' + c + '=' + 0);
+                alert(a + "x2" + "+" + b + "x" + "+" + c + "=" + 0);
                 discriminant = b * b - sides * a * c;
-                alert('discriminant =' + discriminant);
+                alert("discriminant =" + discriminant);
 
                 if (discriminant > 0) {
                     x1 = Math.round((-b + Math.sqrt(discriminant)) / (quadrat * a));
                     x2 = Math.round((-b - Math.sqrt(discriminant)) / (quadrat * a));
-                    console.log('x1 = ' + x1 + ', x2 = ' + x2);
+                    console.log("x1 = " + x1 + ", x2 = " + x2);
                 } else if (discriminant === 0) {
                     x = Math.round(-b / (quadrat * a));
 
-                    console.log(' x = ' + x);
+                    console.log(" x = " + x);
                 } else {
-                    console.log('no solution,discriminant < 0');
+                    console.log("no solution,discriminant < 0");
                 }
             }
         }
