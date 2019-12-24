36d5e8040d02b7d72e619bc4abd5c417b1917dbd
Valeriia-Rez <Valeria09122016@gmail.com>
2019-12-24 13:51:51 +0200

    implement homework N6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,36 +2,36 @@ let discriminant, x, x1, x2, a, b, c;
 const sides = 4;
 const quadrat = 2;
 
-if (confirm("Solve Quadratic equation. Example: ax2 + bx + c = 0")) {
-    a = parseFloat(prompt("Enter number for a ="));
+if (confirm('Solve Quadratic equation. Example: ax2 + bx + c = 0')) {
+    a = parseFloat(prompt('Enter number for a ='));
     if (a === 0 || isNaN(a)) {
-        alert("Invalid input data");
-        console.log("Invalid input data");
+        alert('Invalid input data');
+        console.log('Invalid input data');
     } else {
-        b = parseFloat(prompt("Enter number for b ="));
+        b = parseFloat(prompt('Enter number for b ='));
         if (isNaN(b)) {
-            alert("Invalid input data");
-            console.log("Invalid input data");
+            alert('Invalid input data');
+            console.log('Invalid input data');
         } else {
-            c = parseFloat(prompt("Enter number for c ="));
+            c = parseFloat(prompt('Enter number for c ='));
             if (isNaN(c)) {
-                alert("Invalid input data");
-                console.log("Invalid input data");
+                alert('Invalid input data');
+                console.log('Invalid input data');
             } else {
-                alert(a + "x2" + "+" + b + "x" + "+" + c + "=" + 0);
+                alert(a + 'x2' + '+' + b + 'x' + '+' + c + '=' + 0);
                 discriminant = b * b - sides * a * c;
-                alert("discriminant =" + discriminant);
+                alert('discriminant =' + discriminant);
 
                 if (discriminant > 0) {
                     x1 = Math.round((-b + Math.sqrt(discriminant)) / (quadrat * a));
                     x2 = Math.round((-b - Math.sqrt(discriminant)) / (quadrat * a));
-                    console.log("x1 = " + x1 + ", x2 = " + x2);
+                    console.log('x1 = ' + x1 + ', x2 = ' + x2);
                 } else if (discriminant === 0) {
                     x = Math.round(-b / (quadrat * a));
 
-                    console.log(" x = " + x);
+                    console.log(' x = ' + x);
                 } else {
-                    console.log("no solution,discriminant < 0");
+                    console.log('no solution,discriminant < 0');
                 }
             }
         }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,14 +1,31 @@
 let a, b, c;
 
-if (
-    typeof a === "" ||
-    undefined ||
-    typeof b === "" || undefined ||
-    typeof c === "" || undefined
-) {
-    alert("input values should be ONLY numbers");
-} else {
-    a = parseInt(prompt("a ="));
-    b = parseInt(prompt("b ="));
-    c = parseInt(prompt("c ="));
+if (confirm("Identify triangle type.")) {
+    a = parseInt(prompt("Enter number for tringle side a ="));
+    if (typeof a === "" || typeof a === undefined) {
+        alert("input values should be ONLY numbers");
+    } else {
+        b = parseInt(prompt("Enter number for tringle side b ="));
+        if (typeof b === "" || typeof b === undefined) {
+            alert("input values should be ONLY numbers");
+        } else {
+            c = parseInt(prompt("Enter number for tringle side c ="));
+            if (typeof c === "" || typeof c === undefined) {
+                alert("input values should be ONLY numbers");
+            } else {
+                if (a === 0 || b === 0 || c === 0) {
+                    alert("A triangle must have 3 sides with a positive definite length");
+                } else if (a + b <= c || a + c <= b || b + c <= a) {
+                    alert("Triangle doesn’t exist");
+                    console.log("Triangle doesn’t exist");
+                } else if (a === b && a === c) {
+                    console.log("Equilateral triangle");
+                } else if (a === b || b === c || a === c) {
+                    console.log("Isosceles triangle");
+                } else if (a !== b && a !== c && b !== c) {
+                    console.log("Scalene triangle");
+                }
+            }
+        }
+    }
 }
