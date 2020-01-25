0465e55d39b0772623e13f6932e1254ef206ac44
Valeriia-Rez <Valeria09122016@gmail.com>
2020-01-25 18:11:15 +0200

    implement homework N11

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -32,38 +32,39 @@ const structure = [{
 
 const rootNode = document.getElementById('root');
 
-function generateLiWithContent(liContentText) {
-    const li = document.createElement("li");
-    li.innerHTML = `<a><i class="material-icons">folder</i><span>${liContentText}</span></a>`
+function generateLiWithContent(liContentText, iconName) {
+    const li = document.createElement('li');
+    li.innerHTML = `<div class="select"><i class='material-icons'>${iconName}</i><span>${liContentText}</span></div>`
     return li;
 }
 
 function walkThroughObject(item) {
-    const ul = document.createElement("ul");
+    const ul = document.createElement('ul');
     ul.id = item.title;
-    ul.className = " hide";
+    ul.className = ' hide';
     item.children.forEach(function(innerItemEl) {
         if (innerItemEl.folder && innerItemEl.children) {
-            const li = generateLiWithContent(innerItemEl.title);
+            const li = generateLiWithContent(innerItemEl.title, "folder");
             const test = walkThroughObject(innerItemEl);
             li.appendChild(test);
-            li.addEventListener("click", (e) => {
+            li.addEventListener('click', (e) => {
                 e.stopPropagation();
                 onSectionClick(innerItemEl.title)
             });
             ul.appendChild(li);
-        } else if (innerItemEl.folder && !innerItemEl.children && innerItemEl.children !== "undefined") {
-            const li = generateLiWithContent(innerItemEl.title);
-            const innerUl = document.createElement("ul");
-            innerUl.className = " hide";
+        } else if (innerItemEl.folder && !innerItemEl.children && innerItemEl.children !== 'undefined') {
+            const li = generateLiWithContent(innerItemEl.title, "folder");
+            const innerUl = document.createElement('ul');
+            innerUl.className = ' hide';
             innerUl.id = innerItemEl.title;
 
-            const innerLi = document.createElement("li");
-            innerLi.appendChild(document.createTextNode("Folder is empty"));
-
+            const innerLi = document.createElement('li');
+            innerLi.className = "select";
+            innerLi.appendChild(document.createTextNode('Folder is empty'));
+            innerLi.style.fontStyle = "italic";
             innerUl.appendChild(innerLi);
 
-            li.addEventListener("click", (e) => {
+            li.addEventListener('click', (e) => {
                 e.stopPropagation();
                 onSectionClick(innerItemEl.title);
             });
@@ -71,8 +72,8 @@ function walkThroughObject(item) {
 
             ul.appendChild(li);
         } else {
-            const li = document.createElement("li");
-            li.appendChild(document.createTextNode(innerItemEl.title));
+            const li = generateLiWithContent(innerItemEl.title, "insert_drive_file");
+            li.className = "liFile";
             ul.appendChild(li);
         }
     })
@@ -82,24 +83,28 @@ function walkThroughObject(item) {
 function onSectionClick(id) {
 
     const element = document.getElementById(id);
-    if (element.className.indexOf("show") == -1) {
-        element.className += " show";
+    const parentElem = element.parentElement;
+    const icon = parentElem.querySelector(".material-icons");
+    if (element.className.indexOf('show') === -1) {
+        element.className += ' show';
+        icon.innerText = "folder_open";
     } else {
-        element.className = element.className.replace(" show", " hide");
+        element.className = element.className.replace(' show', ' hide');
+        icon.innerText = "folder";
     }
 }
 
 function createTree() {
-    const testUl = document.createElement("ul");
+    const testUl = document.createElement('ul');
     structure.forEach(function(el) {
-        const li = generateLiWithContent(el.title);
-        li.addEventListener("click", (e) => {
+        const li = generateLiWithContent(el.title, "folder");
+        li.addEventListener('click', (e) => {
             e.stopPropagation();
-            onSectionClick(el.title)
+            onSectionClick(el.title);
+
         });
         const liInnerItems = walkThroughObject(el);
         li.appendChild(liInnerItems);
-
         testUl.appendChild(li);
 
     })
@@ -107,16 +112,4 @@ function createTree() {
 }
 
 
-console.log(createTree());
-let arr = [1, -2, 3, 4];
-
-function getMin(el) {
-    let min = el[0];
-    for (let i = 0; i < el.length; i++) {
-        if (min > el[i]) {
-            min = el[i];
-        }
-    }
-    return min;
-}
-console.log("hh", getMin(arr));
+createTree();
